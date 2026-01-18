b1e498df9567ec0e6562e83259c73a8f36b5c6d8
t-nakatani <tomoya.12@icloud.com>
2026-01-18 22:03:35 +0900

    wip: add_block

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 use chrono::Utc;
-
+use log::error;
 
 pub struct App {
     pub blocks: Vec<Block>,
@@ -35,11 +35,63 @@ impl App {
         };
         self.blocks.push(genesis_block);
     }
+
+    fn try_add_block(&mut self, block: Block) {
+        let last_block = self.blocks.last().unwrap();
+        if self.is_valid_block(last_block, &block) {
+            self.blocks.push(block);
+        }
+
+        error!("Invalid block");
+    }
+
+
+
+    fn is_valid_block(&self, last_block: &Block, block: &Block) -> bool {
+        if block.previous_hash != last_block.hash {
+            return false;
+        }
+
+        if block.timestamp <= last_block.timestamp {
+            return false;
+        }
+
+        true
+    }
 }
 
 
 fn main() {
     let mut app = App::new();
     app.genesis();
+    app.try_add_block(Block {
+        id: 1,
+        hash: Hash::from("0000000000000000000000000000000000000000000000000000000000000001"),
+        previous_hash: Hash::from("0000000000000000000000000000000000000000000000000000000000000000"),
+        timestamp: Utc::now().timestamp() as u64,
+        data: "This is the second block!".to_string(),
+        nonce: 20260119,
+    });
     println!("{:?}", app.blocks);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid_block() {
+        let mut app = App::new();
+        app.genesis();
+        let new_block = Block {
+            id: 1,
+            hash: Hash::from("0000000000000000000000000000000000000000000000000000000000000001"),
+            previous_hash: Hash::from("0000000000000000000000000000000000000000000000000000000000000000"),
+            timestamp: Utc::now().timestamp() as u64 + 1,
+            data: "This is the second block!".to_string(),
+            nonce: 20260119,
+        };
+        assert!(app.is_valid_block(&app.blocks[0], &new_block));
+    }
 }
